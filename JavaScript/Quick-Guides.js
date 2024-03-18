55c7e81f810d4f18595262a92e682272329a161b
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2024-03-18 15:29:24 +0530

    Update Quick-Guides.js

--- JavaScript/Quick-Guides.js
+++ JavaScript/Quick-Guides.js
@@ -1,3 +1,5 @@
 //launch URL in new tab using target="_blank"
 <a href="https://www.google.com" target="_blank"> Open Google </a>
 -----
+// linking css file to html / other sheet
+  <link rel="stylesheet" href="styles.css">
