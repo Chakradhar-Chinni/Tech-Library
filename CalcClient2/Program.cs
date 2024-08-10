c863636d5015256ec08474481a337f8f4d73e22f
ChakradharChinni <chakradhar.chinni@gmail.com>
2024-08-10 23:07:31 +0530

    Pushing CalcClient2 dll source code

---
+++ CalcClient2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcClient2
+{    
+    public class Class1
+    {
+        public int isMax(int num1,int num2)
+        {
+            return Math.Max(num1, num2);
+        }
+        public int isMin(int num1,int num2) 
+        {
+            return Math.Min(num1, num2);
+        }
+    }    
+}
