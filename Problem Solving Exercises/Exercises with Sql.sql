ae571d663db7673ebb0fbb72ee50f6530819e97c
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2024-12-01 16:56:31 +0530

    Create Exercises with Sql.sql

---
+++ Problem Solving Exercises/Exercises with Sql.sql
@@ -0,0 +1,3 @@
+#1 https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem?isFullScreen=true
+  Q: Query a count of the number of cities in CITY having a Population larger than .
+  SELECT COUNT(*) FROM CITY WHERE Population>100000
