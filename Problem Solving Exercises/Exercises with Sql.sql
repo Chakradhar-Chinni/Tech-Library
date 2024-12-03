fc39018dccf860030659e5af80dff9467d0939e3
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2024-12-03 12:24:45 +0530

    Update Exercises with Sql.sql

--- Problem Solving Exercises/Exercises with Sql.sql
+++ Problem Solving Exercises/Exercises with Sql.sql
@@ -7,3 +7,8 @@
   Q:Query the total population of all cities in CITY where the District is California.
   Notes: Use SUM() function to calculate total sum of numeric column
   SELECT SUM(Population) from CITY WHERE DISTRICT='California';
+
+#3 https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
+  Q: Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+  Notes: Using AND Clause
+  SELECT NAME FROM CITY WHERE COUNTRYCODE='USA' AND POPULATION>120000
