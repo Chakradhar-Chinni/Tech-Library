7ad9b71a22ec0b24598c73dd12018d6320f7304d
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2025-06-09 23:11:03 +0530

    Update aspnetCore Web API.cs

--- aspnetcore/aspnetCore Web API.cs
+++ aspnetcore/aspnetCore Web API.cs
@@ -2903,6 +2903,33 @@ namespace CityInfo.API.Controllers
 
 
 
+<h2> delete point of interest
+/*
+DELETE https://localhost:7167/api/cities/1/pointofinterest/4
+Response: 204 No Content
+*/
+
+
+## /Services/ICityInfoRepository.cs
+using CityInfo.API.Entities;
+using CityInfo.API.Models;
+
+namespace CityInfo.API.Services
+{
+    public interface ICityInfoRepository 
+    {
+        Task<IEnumerable<City>> GetCitiesAsync();
+        Task<City?> GetCityAsync(int cityId, bool includePointsOfInterest);
+        Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId);
+        Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId, int pointOfInterestId);
+        Task<bool> CityExistsAsync(int cityId);
+        Task AddPointOfInterestForCityAsync(int cityId, PointOfInterest pointOfInterest);
+        Task<bool> SaveChangesAsync();
+
+        void DeletePointOfInterest(PointOfInterest pointofInterest);
+    }
+}
+
 
 
 
