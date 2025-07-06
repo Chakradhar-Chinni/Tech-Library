3e5e0ebea3f241dc252d0bb85ef5efc18d5928a8
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2025-07-06 20:46:01 +0530

    Update Using Angular Template Syntax.ts

--- Angular/1. Angular Fundamentals/Using Angular Template Syntax.ts
+++ Angular/1. Angular Fundamentals/Using Angular Template Syntax.ts
@@ -1,5 +1,5 @@
 
-<<h2>> Interpolation 
+<<h2>> Interpolation Expressions
 
 {{ }} interplolation syntax can evaluate simple expression, like example below. It's better to not use it for complex calculations
 
@@ -12,3 +12,92 @@
 <bot-catalog> </bot-catalog>
 
   
+<<h2>> Binding to component data with Interpolation
+
+1. product.model.ts - Interface IProduct is created
+2. catalog.component.ts
+    - product variable of type Iproduct is created. 
+    - constructor initializes the data for product
+3. catalog.component.html
+    Interpolation ({{ }}) is used to bind data from the component to the view.
+
+
+
+## /src/app/catalog/product.model.ts
+export interface IProduct
+{
+  id: number;
+  description: string;
+  name: string;
+  imageName: string;
+  category: string;
+  price: number;
+  discount: number;
+}
+
+
+
+## /src/app/catalog/catalog.component.ts
+import { Component, OnInit } from '@angular/core';
+import {IProduct } from './product.model';
+
+@Component({
+  selector: 'bot-catalog',
+  templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.css']
+})
+export class CatalogComponent {
+   product: IProduct;
+
+   constructor()
+   {
+     this.product = {
+      id: 1,
+      description: "A friendly robot with two eyes and a smile -- great for domestic use",
+      name: "Friendly Bot",
+      imageName: "head-friendly.png",
+      category: "Heads",
+      price: 945.0,
+      discount: 0.2,
+     };
+   }
+}
+
+
+
+## /src/app/catalog/catalog.component.html
+<div class="container">
+  <div class="filters">
+    <a class="button">Heads</a>
+    <a class="button">Arms</a>
+    <a class="button">Torsos</a>
+    <a class="button">Bases</a>
+    <a class="button">All</a>
+  </div>
+
+  <ul class="products">
+    <li class="product-item">
+      <div class="product">
+        <div class="product-details">
+          <img src="{{ '/assets/images/robot-parts/' + product.imageName }}" alt="head-friendly.png" />
+          <div class="product-info">
+            <div class="name"> {{ product.name}} </div>
+            <div class="description"> {{product.description}} </div>
+            <div class="category">Part Type: {{product.category}} </div>
+          </div>
+        </div>
+        <div class="price">
+          <div>${{product.price.toFixed(2)}}</div>
+          <button class="cta">Buy</button>
+        </div>
+      </div>
+    </li>
+  </ul>
+</div>
+
+
+
+
+
+
+
