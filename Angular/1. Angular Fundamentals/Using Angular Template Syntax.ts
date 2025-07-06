9107eb627d7e06946dd0759891f1b81af130b0f7
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2025-07-06 18:56:31 +0530

    Interpolation

--- Angular/1. Angular Fundamentals/Using Angular Template Syntax.ts
+++ Angular/1. Angular Fundamentals/Using Angular Template Syntax.ts
@@ -1,1 +1,14 @@
 
+<<h2>> Interpolation 
+
+{{ }} interplolation syntax can evaluate simple expression, like example below. It's better to not use it for complex calculations
+
+
+##/src/app/app.component.html
+    
+<bot-site-header></bot-site-header>
+<p> 2+2= {{2+2}} </p>
+<bot-home>  </bot-home>
+<bot-catalog> </bot-catalog>
+
+  
