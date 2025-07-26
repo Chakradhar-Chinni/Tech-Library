4036bddf964525123a9e9abca258a192aad3e19c
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2025-07-26 15:11:09 +0530

    Create m2. Getting Started.cs

---
+++ aspnetcore/MVC/m2. Getting Started.cs
@@ -0,0 +1,70 @@
+<<h2>> Introduction
+
+DI
+Logger Configuration
+Scaffolding code
+Valdiation
+Model Binding
+MVC - many pieces exploration
+Middlewares - configuring, using
+Filters, Attributes
+
+
+
+
+
+
+
+<<h2>> Version Check
+C# 12
+.nET 8
+VS 2022
+
+
+
+
+
+
+<<h2>> MVC Pattern
+
+Models
+contains user properties 
+
+View
+shows data on presentation layer
+
+
+Controller
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
