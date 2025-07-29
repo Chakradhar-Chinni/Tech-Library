ff2379ecc0dfe2c710206e00f02df08e9b27dd2e
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2025-07-29 19:17:49 +0530

    Update m3-Setting Up MVC Application.cs

--- aspnetcore/MVC aspnetcore/m3-Setting Up MVC Application.cs
+++ aspnetcore/MVC aspnetcore/m3-Setting Up MVC Application.cs
@@ -15,5 +15,23 @@ from VS templates > Choose  > ASP.NET COre Web APP (MVC)
 
 <<h2>> Creating New Project
 
-  1. create ASP.NET COre Empty project
+  1. create ASP.NET Core Empty project
   2. idea is to create everything from scratch with out using pre-defined templates
+
+
+<<h2>> COnfiguring the application
+
+## Program.cs
+var builder = WebApplication.CreateBuilder(args);  //can access appsettings, kestrel, wwwroot, services collection
+builder.Services.AddControllersWithViews(); //lets to use MVC
+var app = builder.Build(); // builds the app
+app.UseStaticFiles(); // middle ware that looks at wwwroot folder
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage(); //exception middleware for dev regions
+}
+
+app.MapDefaultControllerRoute(); // middleware for routes
+app.Run();
+
