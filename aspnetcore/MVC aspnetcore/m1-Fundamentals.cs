baca317f7019ca9f83a7ee3f413aba2dcb1abaad
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2025-07-29 17:49:00 +0530

    Create m1-Fundamentals.cs

---
+++ aspnetcore/MVC aspnetcore/m1-Fundamentals.cs
@@ -0,0 +1,16 @@
+<<h2>>
+aspnetcore 8 (compatible with 6,7,8,9)
+.net 8
+vs 2022
+
+
+
+
+  <<h2>> What is asp.net core
+
+
+
+  <<h2>> Agenda
+  1. develop project from scratch
+  2. ASP.NET Core, Razor Pages, EF Core, Athentication, Testing, Deployment
+  
