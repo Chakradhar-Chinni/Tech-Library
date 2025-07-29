13677a611333aeac1fffc2d070b5ca6a92fbad9b
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2025-07-29 18:42:34 +0530

    Create m3-Setting Up MVC Application.cs

---
+++ aspnetcore/MVC aspnetcore/m3-Setting Up MVC Application.cs
@@ -0,0 +1,19 @@
+<<h2>> Creating New Project
+from VS templates > Choose  > ASP.NET COre Web APP (MVC)
+
+  Explore the initial files created
+
+  main method is replaced by top level statements from .net 6, c#10
+
+  
+  
+  
+  <<h2>> undestnad Kestrel server role
+
+
+
+
+<<h2>> Creating New Project
+
+  1. create ASP.NET COre Empty project
+  2. idea is to create everything from scratch with out using pre-defined templates
