4b8b612414a10ff556dd4080285209fd802091f5
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2025-08-12 17:35:28 +0530

    Update m5-Adding Entity Framework Core to the App.cs

--- aspnetcore/MVC aspnetcore/m5-Adding Entity Framework Core to the App.cs
+++ aspnetcore/MVC aspnetcore/m5-Adding Entity Framework Core to the App.cs
@@ -38,3 +38,82 @@ namespace BethanysPieShop.Models
                 public DbSet<Pie> Pies { get; set; }
         }
 }
+
+
+
+
+
+
+
+<<h2>> Creating the Repository
+
+1. Create PieRepository, CategoryRepository
+2. Program.cs
+       changed to 
+
+
+## Models\PieRepository.cs
+using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieShop.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly BethanysPieShopDbContext _bethanysPieShopDbContext;
+        public PieRepository(BethanysPieShopDbContext bethanysPieShopDbContext)
+        {
+            _bethanysPieShopDbContext = bethanysPieShopDbContext;
+        }
+
+        public IEnumerable<Pie> AllPies
+        {
+            get
+            {
+                return _bethanysPieShopDbContext.Pies.Include(c => c.Category);
+            }
+        }
+
+        public IEnumerable<Pie> PiesOfTheWeek
+        {
+            get
+            {
+                return _bethanysPieShopDbContext.Pies.Include(c=>c.Category).Where(p => p.IsPieOfTheWeek);
+            }
+        }
+
+        public Pie? GetPieById(int PieId)
+        {
+            return _bethanysPieShopDbContext.Pies.Include(c => c.Category).FirstOrDefault(p=>p.PieId==PieId);
+        }
+
+        public IEnumerable<Pie> SearchPies(string searchQuery)
+        {
+            return _bethanysPieShopDbContext.Pies.Include(c => c.Category).Where(p => p.Name.Contains(searchQuery));
+        }       
+    }
+}
+
+
+
+## Models\CategoryRepository.cs
+namespace BethanysPieShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly BethanysPieShopDbContext _bethanysPieShopDbContext;
+        
+        public CategoryRepository(BethanysPieShopDbContext bethanysPieShopDbContext)
+        {
+            _bethanysPieShopDbContext = bethanysPieShopDbContext;
+        }
+
+        public IEnumerable<Category> AllCategories =>       
+          _bethanysPieShopDbContext.Categories.OrderBy(p => p.CategoryName);      
+    }
+}
+
+
+
+##Progam.cs
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IPieRepository, PieRepository>();
