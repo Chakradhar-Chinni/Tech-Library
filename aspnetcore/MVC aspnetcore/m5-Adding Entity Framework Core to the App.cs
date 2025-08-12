2b8a04a329497eb512f5338f9351f458b3433caf
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2025-08-12 14:21:53 +0530

    Update m5-Adding Entity Framework Core to the App.cs

--- aspnetcore/MVC aspnetcore/m5-Adding Entity Framework Core to the App.cs
+++ aspnetcore/MVC aspnetcore/m5-Adding Entity Framework Core to the App.cs
@@ -12,4 +12,29 @@ Seeding data
 
 
 
-<<h2>>
+<<h2>> Adding EF Core to the Applciation
+
+Install Nuget Packages
+use latest stable version
+
+1. Microsoft.EntityFrameworkCore // to use ef if app
+2. Microsoft.EntityFrameworkCore.Tools // to use migrations
+3. Microsoft.EntityFrameworkCore.SqlServer // to use sql
+
+
+
+
+## \Models\BethanysPieShopDbContext.cs
+
+using Microsoft.EntityFrameworkCore;
+namespace BethanysPieShop.Models
+{
+       public class BethanysPieShopDbContext : DbContext
+        {
+            public BethanysPieShopDbContext(DbContextOptions<BethanysPieShopDbContext> options) : base(options)
+            {
+            }
+                public DbSet<Category> Categories { get; set; }
+                public DbSet<Pie> Pies { get; set; }
+        }
+}
