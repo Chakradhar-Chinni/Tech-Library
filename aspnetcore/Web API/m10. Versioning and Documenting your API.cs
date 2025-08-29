3433a959da1970dd34695375c05129f55ac6a23d
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2025-08-29 20:55:57 +0530

    Update m10. Versioning and Documenting your API.cs

--- aspnetcore/Web API/m10. Versioning and Documenting your API.cs
+++ aspnetcore/Web API/m10. Versioning and Documenting your API.cs
@@ -1,4 +1,4 @@
-<h2> Versioning your API
+<<h2>> Versioning your API
   - As, API keeps evolving. set of users might still require old API while others require updated ones. Versioning helps here.
   - URI versioning is most used these days
 
@@ -32,3 +32,51 @@
    - Example:       
      Accept: application/json; version=1
      
+
+
+       
+
+
+
+<<h2>> Supporting Versioing
+1. Install Nuget Package asp.versioning.mvc
+2. temporarily comment [Authorize] on controllers 
+
+
+
+## Program.cs
+
+using Asp.Versioning;
+
+builder.Services.AddApiVersioning(setupAction =>
+{
+    setupAction.ReportApiVersions = true;
+    setupAction.AssumeDefaultVersionWhenUnspecified = true;
+    setupAction.DefaultApiVersion = new ApiVersion(1, 0);
+}).AddMvc();
+
+
+After hitting any endpoint, Response headers looks like below
+
+content-length	185
+content-type	application/json; charset=utf-8
+date	Fri, 29 Aug 2025 15:13:05 GMT
+server	Kestrel
+x-pagination	{"TotalItemCount":3,"TotalPageCount":1,"PageSize":10,"CurrentPage":1}
+api-supported-versions	1.0
+
+  /*
+note: if setupAction.ReportApiVersions = true; is only given, then following error occurs
+
+{
+  "type": "https://docs.api-versioning.org/problems#unspecified",
+  "title": "Unspecified API version",
+  "status": 400,
+  "detail": "An API version is required, but was not specified."
+}
+*/
+
+
+<<h2>> Versioning your API
+
+
