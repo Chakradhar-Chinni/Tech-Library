4f6f86b9c6467f512ba06197969a8691f8f6069d
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2025-08-30 13:55:57 +0530

    Update m10. Versioning and Documenting your API.cs

--- aspnetcore/Web API/m10. Versioning and Documenting your API.cs
+++ aspnetcore/Web API/m10. Versioning and Documenting your API.cs
@@ -77,6 +77,104 @@ note: if setupAction.ReportApiVersions = true; is only given, then following err
 */
 
 
+
+
+
+
+
+  
+
 <<h2>> Versioning your API
 
+1. CitiesController.cs
+  [ApiVersion] is added
+  as default version is assumed when version is not specified in URI, both URLs works
+
+2. PointOfInterestcontroller.cs
+  - two Api Versions are added
+  - onlu mentioned api versions can access, other api versions cannot access and gives error as "Unspecified Api Version". for detail error look at previous module
+
+3. Files Controller.cs
+  - ApiVersion is marked as deprecated
+
+- look at response headers for all three files
+
+/*
+https://localhost:7167/api/cities?api-version=1
+https://localhost:7167/api/cities
+Response Headers
+content-length	185
+content-type	application/json; charset=utf-8
+date	Sat, 30 Aug 2025 08:09:46 GMT
+server	Kestrel
+x-pagination	{"TotalItemCount":3,"TotalPageCount":1,"PageSize":10,"CurrentPage":1}
+api-supported-versions	1.0
+*/
+
+## CitiesController.cs
+using Asp.Versioning;
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    //[Route("api/cities")]
+    //[Authorize]
+    [Route("api/[controller]")]
+    [ApiVersion(1)]
+    public class CitiesController : ControllerBase
+    {
+
+
+/*
+https://localhost:7167/api/cities/2/pointofinterest?api-version=2
+Response Headers
+content-length	246
+content-type	application/json; charset=utf-8
+date	Sat, 30 Aug 2025 08:09:59 GMT
+server	Kestrel
+api-supported-versions	2.0, 3.0
+*/
+
+## PointOfInterestController.cs
+using Asp.Versioning;
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    //[Authorize(Policy= "MustBeFromChicago")]
+    [Route("/api/cities/{cityId}/pointofinterest")]
+    [ApiVersion(2)]
+    [ApiVersion(3)]
+    public class PointOfInterestController : ControllerBase
+    {
+
+
+
+/*
+https://localhost:7167/api/files/1?api-version=0.5
+Response Headers
+content-length	2951831
+content-type	application/pdf
+date	Sat, 30 Aug 2025 08:10:02 GMT
+server	Kestrel
+content-disposition	attachment; filename=FSD-MERN-Stack-brochure.pdf; filename*=UTF-8''FSD-MERN-Stack-brochure.pdf
+api-deprecated-versions	0.5
+*/
+
+## FilesController.cs
+using Asp.Versioning;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    //[Authorize]
+    [Route("api/files")]
+    [ApiVersion(0.5,Deprecated=true)]
+    public class FilesController : ControllerBase
+
+
+
+
+
+
+
+<<h2>> Supporting Versioned Routes
 
