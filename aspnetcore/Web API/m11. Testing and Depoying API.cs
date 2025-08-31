9fc1f13c7acfcb2a5c368d486c1d89dc488a466b
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2025-08-31 17:40:43 +0530

    Create m11. Testing and Depoying API.cs

---
+++ aspnetcore/Web API/m11. Testing and Depoying API.cs
@@ -0,0 +1,8 @@
+<<h2>> Testing API End Points
+
+Non-Microsoft Ways:
+HTTP_REPL: 
+  - REPL: Read Eval Print Loop
+  - a CLI tool to test API end points and inspect responses
+.http files:
+  - released in .net 8
