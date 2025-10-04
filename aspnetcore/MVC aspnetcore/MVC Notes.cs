3b49e64d166de41a3195e28c3ca226dd3be026a9
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2025-10-04 13:38:15 +0530

    Update MVC Notes.cs

--- aspnetcore/MVC aspnetcore/MVC Notes.cs
+++ aspnetcore/MVC aspnetcore/MVC Notes.cs
@@ -1,5 +1,13 @@
-(1)
+(1) _layouts
 
 1. Multiple Layouts: You can create multiple layout files (e.g., `_Layout.cshtml`, `_PremiumLayout.cshtml`) in `/Views/Shared` and use them selectively in views or via `_ViewStart.cshtml`.
 
 2. Scoped `_ViewStart.cshtml`: Place a `_ViewStart.cshtml` inside folders like `/Views/Premium/` to automatically apply a specific layout (e.g., `_PremiumLayout`) to all views in that folder, overriding the global layout.
+
+
+(2) Sections
+
+Purpose: Sections allow views to inject content (like scripts or styles) into specific placeholders in a shared layout, keeping layout structure clean and reusable.
+
+Scope: Sections are scoped to the view where they’re defined and only rendered when the layout includes a matching @RenderSection() — they are not shared across views.  
+  
