64f3fe3fb9d39d441daa658b1573d2b802a26d6e
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2025-10-13 19:19:14 +0530

    Create HomeController.cs

---
+++ aspnetcore/dot net core/Middleware/Level 1 Example/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using Core.Features.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.Features.Controllers
+{
+    [Route("api/home")]
+    //[Route("[controller]")]
+    public class HomeController : ControllerBase,IDisposable
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //return Ok(result);
+            return Ok();
+        }
+        public void Dispose()
+        {
+            Console.WriteLine("HomeController instance Destroyed ");
+        }
+    }
+}
