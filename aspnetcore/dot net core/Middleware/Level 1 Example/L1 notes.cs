f1738d35fff963a897b651d29b5300b07bd57985
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2025-10-13 19:21:41 +0530

    Create L1 notes.cs

---
+++ aspnetcore/dot net core/Middleware/Level 1 Example/L1 notes.cs
@@ -0,0 +1,6 @@
+output: 
+First MiddleWare: Before next()
+Second MiddleWare: Before next()
+Third MiddleWare: Handling request (pipeline ends here)
+Second MiddleWare: After next()
+First MiddleWare: After next()
