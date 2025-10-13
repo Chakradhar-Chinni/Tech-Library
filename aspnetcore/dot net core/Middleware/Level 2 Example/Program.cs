658fa3da89fe056f2f182208f48497327bbadf72
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2025-10-13 21:10:50 +0530

    Update Program.cs

--- aspnetcore/dot net core/Middleware/Level 2 Example/Program.cs
+++ aspnetcore/dot net core/Middleware/Level 2 Example/Program.cs
@@ -1,1 +1,33 @@
+using Core.Features.Services;
+using Core.Features.Middlewares;
+using Microsoft.AspNetCore.Http;
 
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+/*
+ * COnfigure Custom Middlewares
+ */
+app.UseMiddleware<Core.Features.Middlewares.LoggingMiddleware>();
+app.UseMiddleware<Core.Features.Middlewares.ExceptionHandlingMiddleware>();
+app.UseMiddleware<Core.Features.Middlewares.RequestTimingMiddleware>();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
