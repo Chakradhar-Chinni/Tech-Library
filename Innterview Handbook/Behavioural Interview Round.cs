b2c7b5fd15f1e1d7865248e7d683c080ddec5c69
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2025-10-28 23:03:47 +0530

    Create Behavioural Interview Round.cs

---
+++ Innterview Handbook/Behavioural Interview Round.cs
@@ -0,0 +1,8 @@
+Answer all questions in STAR format
+S - Situation
+T - Task 
+A - Action 
+R - Result
+
+Describe the situation and the task. Exmplain what action was taken, result of the action
+Communicate in positive approach (avoid negative or blaming scenario)
