553bc45765352cd8022900fed92cc48667015392
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2026-01-01 20:38:10 +0530

    Update ProductController.cs

--- aspnetcore/dot net core/Caching/m3_Server Side Caching/m3.1_Basic Example/ProductController.cs
+++ aspnetcore/dot net core/Caching/m3_Server Side Caching/m3.1_Basic Example/ProductController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using REST_API_Caching.Controllers.Models;
 
 namespace REST_API_Caching.Controllers
@@ -6,32 +7,55 @@ namespace REST_API_Caching.Controllers
     [Route("api/[controller]")]
     public class ProductController : Controller
     {
+        private readonly IMemoryCache _cache;
+
+        public ProductController(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
         [HttpGet("premiumProducts")]
-        [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Client, NoStore = false)]
         public IActionResult GetPremiumProducts()
         {
+            const string cacheKey = "premiumProducts";
             
-            Console.WriteLine(HttpContext.GetEndpoint());
-            Console.WriteLine(HttpContext.Request);
-            var premiumProductsList = new List<ProductInfo>();
-
-            premiumProductsList.Add(new ProductInfo
+            // Try to get cached data
+            if (!_cache.TryGetValue(cacheKey, out List<ProductInfo> premiumProductsList))
             {
-                ProductId = 1,
-                ProductName = "M1 Chip",
-                ProductDescription = "M1 chip is advanced processew",
-                ProductCategory = "Apple Hardware",
+                Console.WriteLine("Cache MISS - Generating data");
+                
+                // Data not in cache, create it
+                premiumProductsList = new List<ProductInfo>();
 
-            });
+                premiumProductsList.Add(new ProductInfo
+                {
+                    ProductId = 1,
+                    ProductName = "M1 Chip",
+                    ProductDescription = "M1 chip is advanced processew",
+                    ProductCategory = "Apple Hardware",
+                });
 
-            premiumProductsList.Add(new ProductInfo
+                premiumProductsList.Add(new ProductInfo
+                {
+                    ProductId = 2,
+                    ProductName = "M2 Chip",
+                    ProductDescription = "M2 chip is advanced processer",
+                    ProductCategory = "Apple Hardware",
+                });
+
+                // Set cache options
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetAbsoluteExpiration(TimeSpan.FromSeconds(120));
+                    //.SetSlidingExpiration(TimeSpan.FromSeconds(30));
+
+
+                // Save data in cache
+                _cache.Set(cacheKey, premiumProductsList, cacheEntryOptions);
+            }
+            else
             {
-                ProductId = 2,
-                ProductName = "M2 Chip",
-                ProductDescription = "M2 chip is advanced processer",
-                ProductCategory = "Apple Hardware",
-
-            });
+                Console.WriteLine("Cache HIT - Returning cached data");
+            }
 
             return Ok(premiumProductsList);
         }
