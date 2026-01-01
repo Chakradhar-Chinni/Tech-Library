1c157cd30acfcd18424ac639008c4d9b5b2be837
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2026-01-01 20:09:38 +0530

    Create Program.cs

---
+++ aspnetcore/dot net core/Caching/m2_Client Side Caching/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddResponseCaching();  //new
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Enable response caching middleware //new
+app.UseResponseCaching();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
